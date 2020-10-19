1787faf91855d9a5160915472f5aa8ec2b87ef87
Heinz-Th-Ch <69845390+Heinz-Th-Ch@users.noreply.github.com>
2020-10-19 09:26:14 +0200

    data file keys added

--- src/main/java/dataStructures/CommonValues.java
+++ src/main/java/dataStructures/CommonValues.java
@@ -8,6 +8,25 @@ public class CommonValues {
     public static final String PROPERTY_LOG_FILE_PATH = "logfile.path";
     public static final String PROPERTY_LOG_FILE_NAME = "logfile.filename";
 
+    public static final String PROPERTY_INTERNAL_SERVER_PORT = "internal.server.port";
+    public static final String PROPERTY_INTERNAL_SERVER_NUMBER_OF_PARALLEL_CONNECTS = "internal.server.number.of.parallel.connects";
+    public static final String PROPERTY_INTERNAL_SERVER_ACCEPTED_HOSTS = "internal.server.accepted.hosts";
+
+    public static final String PROPERTY_WEB_SERVER_PORT = "web.server.port";
+    public static final String PROPERTY_WEB_SERVER_NUMBER_OF_PARALLEL_CONNECTS = "web.server.number.of.parallel.connects";
+    public static final String PROPERTY_WEB_SERVER_ACCEPTED_HOSTS = "web.server.accepted.hosts";
+
+    public static final String PROPERTY_REMOTEAPPLICATION_INTERNAL_SERVER_HOSTNAME = "remoteapplication.internal.server.hostname";
+    public static final String PROPERTY_REMOTEAPPLICATION_INTERNAL_SERVER_PORT = "remoteapplication.internal.server.port";
+
+    public static final String PROPERTY_WEBAPPLICATION_INTERNAL_SERVER_HOSTNAME = "webapplication.internal.server.hostname";
+    public static final String PROPERTY_WEBAPPLICATION_INTERNAL_SERVER_PORT = "webapplication.internal.server.port";
+
+    public static final String PROPERTY_DATA_FILE_PATH = "datafile.path";
+    public static final String PROPERTY_DATA_BALL_CHARACTERISTICS_FILE_NAME = "datafile.ballcharacteristics.filename";
+    public static final String PROPERTY_DATA_CRAZY_GOLF_SITE_CHARACTERISTICS_FILE_NAME = "datafile.crazygolfsitecharacteristics.filename";
+    public static final String PROPERTY_DATA_SUITCASE_CHARACTERISTICS_FILE_NAME = "datafile.suitcasecharacteristics.filename";
+
     public static final String DIRECTORY_PLACE_HOLDER = "{&workinglevel}";
     public static final String TIME_STAMP_PLACE_HOLDER = "{&timestamp}";
 
